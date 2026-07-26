61408ace5fb406484e9e632271cf9ace81ec5de4
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-07-26 19:35:22 +0530

    fix(root.go): correct 'goshare' typo to 'GoShare'

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,7 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "goshare",
+	Use:   "GoShare",
 	Short: "GoShare is a Go CLI tool that can share files at the maximum speed possible in a LAN",
 	Long:  "GoShare is made for local area networks and is highly secure while sharing files across different OS environments like Mac, Windows, Linux, Android, iOS, etc.",
 	Run: func(cmd *cobra.Command, args []string) {
