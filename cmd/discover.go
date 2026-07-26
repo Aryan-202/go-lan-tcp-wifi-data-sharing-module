b5d1271476de7a067d99007c40840de06b2a9ae7
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-07-26 19:52:31 +0530

    feat: add discover command this will help finding nearby devices having goshare turned on

---
+++ cmd/discover.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var discoverCmd = &cobra.Command{
+	Use:   "discover",
+	Short: "Discover nearby GoShare devices",
+	Long: `Search the local network for GoShare peers usingz
+the configured discovery protocols.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("Searching for nearby devices...")
+
+		// TODO: Call internal/discovery package
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(discoverCmd)
+}
