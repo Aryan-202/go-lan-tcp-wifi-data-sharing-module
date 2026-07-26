aa3534c92a5bc1d680848afcfc38651727696dad
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-07-26 19:15:26 +0530

    feat: implement CLI with cobra and update dependencies in go.mod

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("Hello");
+	rootCmd := &cobra.Command{
+		Use:   "app",
+		Short: "A small CLI",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello")
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
