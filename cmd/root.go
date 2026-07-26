6c32c77641f5d333c5bd2e1e2f2dbc0a04dc00e0
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-07-26 19:33:00 +0530

    feat: refactor main function and implement CLI command structure

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "goshare",
+	Short: "GoShare is a Go CLI tool that can share files at the maximum speed possible in a LAN",
+	Long:  "GoShare is made for local area networks and is highly secure while sharing files across different OS environments like Mac, Windows, Linux, Android, iOS, etc.",
+	Run: func(cmd *cobra.Command, args []string) {
+		
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Printf("Error while executing zero command '%s'\n", err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,23 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	
 
-	"github.com/spf13/cobra"
+	"github.com/Aryan-202/go-lan-tcp-wifi-data-sharing-module/cmd"
+	
 )
 
 func main() {
-	rootCmd := &cobra.Command{
-		Use:   "app",
-		Short: "A small CLI",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello")
-		},
-	}
-
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
